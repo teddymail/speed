603292466840c2d3c566ac9ddb1ed25eac23f6db
Josins <774542602@qq.com>
2017-08-18 15:18:43 +0800

    增加supjos\net网络模块

--- App.php
+++ App.php
@@ -5,16 +5,41 @@ namespace supjos;
 class App
 {
 
+    public static $aliases = [
+        '@supjos' => __DIR__
+    ];
+
     /**
      * The autoload function to load the needed class
      * @param type $className
      */
     public static function autoload($className)
     {
-        echo $className;
+        if (!empty($className)) {
+
+            $parentPath = '';
+
+            $slashPos = strpos($className, '\\');
+            if ($slashPos === FALSE) {
+                $nameAlias = $className;
+            } else {
+                $nameAlias = substr($className, 0, $slashPos);
+            }
+            foreach (static::$aliases as $name => $dir) {
+                if (strcmp($name, '@' . $nameAlias) == 0) {
+                    $parentPath = $dir;
+                    break;
+                }
+            }
+            $filePath = strtr($parentPath . substr($className, $slashPos), '\\', '/') . '.php';
+            if (file_exists($filePath)) {
+                require_once($filePath);
+            } else {
+                exit("Class File {$filePath} Not Found");
+            }
+        }
     }
 
 }
-
 
 spl_autoload_register(['supjos\App', 'autoload']);
---
+++ net/Net.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace supjos\net;
+
+class Net
+{
+
+    public function getVersion()
+    {
+        return '1.1.0';
+    }
+
+}
